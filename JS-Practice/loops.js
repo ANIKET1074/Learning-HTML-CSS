76250b461cfbca3182298a7ed64156a89c32e7f6
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-27 17:16:41 +0530

    loops practice work added

---
+++ JS-Practice/loops.js
@@ -0,0 +1,19 @@
+// There are 3 types of loops
+
+// 1) While loop
+//  -> Syntax
+// while (i < Array.length) {
+//   console.log(array[i]);
+// }
+
+// 2) For loop
+// -> Syntax
+// for (let index = 0; index < array.length; index++) {
+//   console.log(array[index]);
+// }
+
+// 3) do while :- it executes atleast once...
+// -> Syntax
+// do{
+// code here
+// }while(condition)
