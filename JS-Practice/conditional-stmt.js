a093f1db0017dad660b9b8a5b4766e4b7fd45e0a
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-27 17:15:11 +0530

    conditional stmt practice added

---
+++ JS-Practice/conditional-stmt.js
@@ -0,0 +1,25 @@
+// there are 3 types of conditional statements
+
+// 1) if-else
+// 2) switch
+// 3) ternary operator
+
+// 1) if-else
+// if(condition){
+// code
+// }
+// else{
+// code
+// }
+
+// 2) switch (variable) {
+// case value:
+// code
+// break;
+
+// default:
+// break;
+// }
+
+// 3) ternary operator
+// condition ? stmt1 : stmt2   if condition is true then stmt1 will execute otherwise stmt2 will execute
