737c7428eb1217b02ac1ec91eaa85f95840fe225
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-27 17:17:12 +0530

    rock paper scissor game using if else added

--- JS-Practice/main.js
+++ JS-Practice/main.js
@@ -46,21 +46,3 @@
 // } else {
 //   alert("ok, may be we play next time..");
 // }
-
-// Functions
-
-// Functions declaration Syntax
-
-function getUserName(email) {
-  return email.slice(0, email.indexOf("@"));
-}
-
-console.log(getUserName("userName@gmail.com"));
-
-// Functions provides reusable code
-
-const toProperCase = (name) => {
-  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-};
-
-console.log(toProperCase("anIKet"));
