8494145fd9cb48a86be0210b579f09dfe305cc1a
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-29 15:02:21 +0530

    date and methods in date practice

---
+++ JS-Practice/date.js
@@ -0,0 +1,33 @@
+const dt = new Date("2023-09-29");
+const dt1 = new Date("2023-09-29T12:00:00Z");
+
+// differnt ways to write the date
+// new Date()
+// new Date(date string)
+
+// new Date(year,month)
+// new Date(year,month,day)
+// new Date(year,month,day,hours)
+// new Date(year,month,day,hours,minutes)
+// new Date(year,month,day,hours,minutes,seconds)
+// new Date(year,month,day,hours,minutes,seconds,ms)
+
+// Date Get methods
+
+const dt3 = new Date();
+console.log(dt3.getFullYear());
+console.log(dt3.getDate());
+console.log(dt3.getDay());
+console.log(dt3.getHours());
+console.log(dt3.getMilliseconds());
+console.log(dt3.getMonth());
+
+// getFullYear();
+// getMonth();
+// getDate();
+// getDay();
+// getHours();
+// getMinutes();
+// getSeconds();
+// getMilliseconds();
+// getTime();
