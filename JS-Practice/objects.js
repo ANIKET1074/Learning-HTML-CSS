c3ef8ca4e47d2173c68d5a78d7621896b1ad8f29
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-30 15:02:46 +0530

    object in javascript practice work added

---
+++ JS-Practice/objects.js
@@ -0,0 +1,23 @@
+const student = {
+  firstname: "Aniket",
+  lastname: "jadav",
+  stream: "CE",
+  city: "Navsari",
+  mobile: 4856898867,
+};
+
+console.log(student);
+console.log(student["lastname"]);
+console.log(student.firstname);
+
+// This keyword
+
+// it refers to the owner of the function
+// we can write inside the function i.e. this.firstname
+// syntax :- this.methodname
+
+// we can access object methods
+// objectname.methodName();
+
+// e.g.
+console.log(student.firstname);
