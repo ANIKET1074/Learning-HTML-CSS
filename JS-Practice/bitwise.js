f67f8130a64cdfc559e804585ac437abcc8d7d88
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-30 15:02:17 +0530

    bitwise operator practice work added

---
+++ JS-Practice/bitwise.js
@@ -0,0 +1,23 @@
+// Bitwise operators and , or , not, xor, left shift, right shift
+//  & - and , | - or , ~ - not, ^ - xor, >> - signed right shift , << zero fill left shift, >>> - zero fill right shift
+
+let x = 5 & 1;
+console.log(x);
+
+let y = ~5;
+console.log(y);
+
+let z = 5 | 1;
+console.log(z);
+
+let p = 5 ^ 6;
+console.log(p);
+
+let q = 5 << 1;
+console.log(q);
+
+let r = -6 >> 1;
+console.log(r);
+
+let s = 7 >>> 1;
+console.log(s);
