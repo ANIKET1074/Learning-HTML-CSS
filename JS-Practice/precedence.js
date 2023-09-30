624b45ec6a83ba8af334f24963cadabd3ea9ee87
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-30 15:03:21 +0530

    precedence order in javascript practice

---
+++ JS-Practice/precedence.js
@@ -0,0 +1,7 @@
+const x = 100 + 50 * 3;
+console.log(x);
+
+const y = (100 + 50) * 3;
+console.log(y);
+
+// Operator precedence :- brackets and dot has highest precedence in js , then incremental operator , not operator, unary operator, Arithmatic operator, shift operator, relational operator, comparison operator, Bitwise operator, logical operator, conditional operators, Assignment operator
