067068ecc050bc88c6399c1815bbfa8432b08d22
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-10-04 15:17:18 +0530

    js-modules work added

---
+++ JS-Practice/js-modules.js
@@ -0,0 +1,31 @@
+// JS module allows you to break the code into seperate lines
+// This makes easier to maintain the code-base
+
+// Modules are imported from external files with the import statement
+// Modules also rely on type = "module" in the <script> tag.
+
+// examples for import the module
+{
+  /* <script type="module">
+import message from "./message.js";
+</script> */
+}
+
+// Named Export
+// We can create export using 2 ways
+
+// 1) Inline export const name = "Aniket";
+// 2) ALl at once at the bottom
+// const name = "ANiket"
+// const sname = "Jadav"
+// export {name,sname}
+
+// default exports
+// export default moduleName
+
+// Import the modules
+// using named exports
+// import {name,sname} from "./modules";
+
+// using default export
+// import modules from "./modules"
