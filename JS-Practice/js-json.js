d8fe9c98ff77b726f78910d5b901b894bac468bf
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-10-05 15:07:09 +0530

    json practice file added

--- JS-Practice/js-json.js
+++ JS-Practice/js-json.js
@@ -1,1 +1,18 @@
 // JSON is a format for storing and transporting data.
+// {
+// 	"students":[
+// 		{"name":"Aniket", "year":2002},
+// 		{"name":"Aniket", "year":2002},
+// 		{"name":"Aniket", "year":2002},
+// 	]
+// }
+
+// Converting the json text to js object
+let text =
+  '{ "employees" : [' +
+  '{ "firstName":"John" , "lastName":"Doe" },' +
+  '{ "firstName":"Anna" , "lastName":"Smith" },' +
+  '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+const obj = JSON.parse(text);
+console.log(obj);
