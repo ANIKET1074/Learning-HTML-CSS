1899a0ede24a7e748b2b1194090edd43ed29e8f6
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-10-20 19:08:27 +0530

    js file practice with navigation, nodes,collection, childNodes

--- JS-Practice/DOM-Practice/index.js
+++ JS-Practice/DOM-Practice/index.js
@@ -5,9 +5,10 @@ console.log(view1);
 const view2 = document.querySelector("#view1");
 console.log(view2);
 
-const elements = document.getElementsByClassName("element");
+const elements = document.getElementsByClassName(".element");
 console.log(elements);
 
+const div = document.querySelector("#view2");
 const divs = document.querySelectorAll(".element");
 console.log(divs);
 
@@ -32,3 +33,43 @@ header.innerHTML = `<h1>Hello Guys</h1><p>This is the text</p>`;
 header.style.display = "flex";
 header.style.justifyContent = "space-between";
 header.style.alignItems = "center";
+
+console.log(evenDivs[0].parentElement);
+console.log(evenDivs[0].parentElement.children);
+console.log(evenDivs[0].parentElement.childNodes);
+console.log(evenDivs[0].parentElement.hasChildNodes());
+console.log(evenDivs[0].parentElement.lastChild);
+console.log(evenDivs[0].parentElement.lastElementChild);
+console.log(evenDivs[0].parentElement.firstChild);
+console.log(evenDivs[0].parentElement.firstElementChild);
+console.log(evenDivs[0].nextSibling);
+console.log(evenDivs[0].nextElementSibling.nextElementSibling);
+console.log(evenDivs[0].previousSibling);
+console.log(evenDivs[0].previousElementSibling);
+
+// using this we can insert or remove the elements
+
+// 1) removing the element
+// while(view1.lastChild){
+// 	view1.lastChild.remove();
+// }
+
+// 2) Inserting the new elements
+const createDivs = (parent, iter) => {
+  const newDiv = document.createElement("div");
+  newDiv.textContent = iter;
+  newDiv.style.backgroundColor = "#000";
+  newDiv.style.color = "#fff";
+  newDiv.style.height = "5rem";
+  newDiv.style.width = "5rem";
+  newDiv.style.display = "flex";
+  newDiv.style.justifyContent = "center";
+  newDiv.style.alignItems = "center";
+  parent.append(newDiv);
+};
+
+// createDivs(div, 13);
+
+for (let i = 13; i < 19; i++) {
+  createDivs(div, i);
+}
